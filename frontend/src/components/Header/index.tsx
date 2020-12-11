fc6b490cecca9e779669ad3e4330adb5e5c16bcb
Isaac Allef <isaac_allef@hotmail.com>
2020-12-11 19:21:33 -0300

    botao de sair

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,13 +3,23 @@ import logo from '../../assets/logo-terapias.png';
 import localLogo from '../../assets/brasao.png';
 
 import { Container } from './styles';
+import { Button } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+    const navigate = useNavigate();
     return (
         <Container>
             <img id='Logo' src={logo} />
             <h1>E-terapias</h1>
             <img id='LocalLogo' src={localLogo} />
+            <Button
+                onClick={() => {
+                    localStorage.clear()
+                    navigate('/')
+                }}
+                style={{WebkitTextFillColor: 'white'}}
+            >Sair</Button>
         </Container>
     );
 };
